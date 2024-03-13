d5dbcf357aafa24370995d8f12b9bd8d45477b89
Bruce <bruce.h.ruan@gmail.com>
2024-03-13 14:52:51 -0400

    keys + secrets

---
+++ main.py
@@ -0,0 +1,9 @@
+import tweepy
+import pandas as pd
+
+consumer_key = "f6D4c1zDljJNtfkNHfmZhWYct"
+consumer_secret = "En4E9LI0Iv8SktdNcnCiRyq0LhS72hZRM4eouGAqZzFxwSS7gn"
+
+access_token = "917067322082213888-cpbY2VeaDMq02nccahoCxV9iye8AXF8"
+access_token_secret = "bjMwjFH9WLLBWSkecXGHG6VK7kYi05xNwHvrVADChBvI4"
+
