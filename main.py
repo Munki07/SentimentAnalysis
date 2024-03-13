374f87917a3e66debb65aced50b68f9bf91d0c23
Bruce <bruce.h.ruan@gmail.com>
2024-03-13 14:55:18 -0400

    pass in twitter API authentication key

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ consumer_secret = "En4E9LI0Iv8SktdNcnCiRyq0LhS72hZRM4eouGAqZzFxwSS7gn"
 access_token = "917067322082213888-cpbY2VeaDMq02nccahoCxV9iye8AXF8"
 access_token_secret = "bjMwjFH9WLLBWSkecXGHG6VK7kYi05xNwHvrVADChBvI4"
 
+#Pass in our twitter API authentication key
+auth = tweepy.OAuth1UserHandler(
+    consumer_key, consumer_secret,
+    access_token, access_token_secret
